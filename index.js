2b0cb4d4b39f4b556db4dd2c91cd083ee4682b98
Lucas Mendoza <mendozalucas001@gmail.com>
2023-12-18 00:23:05 -0300

    cambios

--- index.js
+++ index.js
@@ -6,24 +6,37 @@ class ProductManager{
     }
 
     getProductById(id){
+        const prod = this.products.find((p) => p.id === id)
 
+        if(prod){
+            return prod;
+        }else{
+            console.log('Not found');
+        }
     }
 
     addProduct(title, description, price, thumbnail, code, stock){
-        const prod = {
-            id: ProductManager.id,
-            title: title,
-            description: description,
-            price: price,
-            thumbnail: thumbnail,
-            code: code,
-            stock: stock
+
+        const existeCode = this.products.find((p) => p.code === code);
+
+        if(!existeCode){
+            const prod = {
+                id: ProductManager.id,
+                title: title,
+                description: description,
+                price: price,
+                thumbnail: thumbnail,
+                code: code,
+                stock: stock
+            }
+    
+            this.products.push(prod);
+    
+            ProductManager.id++;
+            console.log('Producto agregado ☑️');
+        }else{
+            console.log('El código que ingresó pertenece a un producto existente');
         }
-
-        this.products.push(prod);
-
-        ProductManager.id++;
-        console.log('Producto agregado ☑️');
     }
 
     getProducts(){
@@ -47,7 +60,12 @@ class ProductManager{
 }
 
 const productos = new ProductManager(); 
-productos.addProduct('Acer Gamer OwO','i5 8gb 265gb 4070ti', 950000,"UrlImagen", 150443, 5);
+/* productos.addProduct('Acer Gamer OwO','i5 8gb 265gb 4070ti', 950000,"UrlImagen", 150443, 5);
 productos.addProduct('HP Gamer OwO','i7 8gb 512gb 4060ti', 750000,"UrlImagen", 150453, 5);
 
-console.log(productos.getProducts());
+console.log(productos.getProducts()); */
+
+let opcion = prompt("Ingrese una opción");
+while(opcion != 4){
+    opcion = prompt("Ingrese una opción");
+} 
