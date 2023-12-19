43c455961575c45517b5cadcec01daf57fd67b66
Lucas <mendozalucas001@gmail.com>
2023-12-19 02:59:04 -0300

    menú

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ class ProductManager{
         const prod = this.products.find((p) => p.id === id)
 
         if(prod){
-            return prod;
+            console.log(prod);
         }else{
             console.log('Not found');
         }
@@ -17,9 +17,9 @@ class ProductManager{
 
     addProduct(title, description, price, thumbnail, code, stock){
 
-        const existeCode = this.products.find((p) => p.code === code);
+        const existsCode = this.products.find((p) => p.code === code);
 
-        if(!existeCode){
+        if(!existsCode){
             const prod = {
                 id: ProductManager.id,
                 title: title,
@@ -41,31 +41,41 @@ class ProductManager{
 
     getProducts(){
         return this.products;
-    }
-
-        /*     mostrarProductos(){
-        this.products.length ?         this.products.forEach(product => {
-            console.log(
-                `--
-                 ${product.id}
-                 ${product.title}
-                 ${product.description}
-                 ${product.price}
-                 ${product.code}
-                 ${product.stock}
-                 --`)
-         }) :
-         console.log("No hay productos.")
-    } */
+    } 
 }
 
 const productos = new ProductManager(); 
-/* productos.addProduct('Acer Gamer OwO','i5 8gb 265gb 4070ti', 950000,"UrlImagen", 150443, 5);
-productos.addProduct('HP Gamer OwO','i7 8gb 512gb 4060ti', 750000,"UrlImagen", 150453, 5);
+productos.addProduct('Acer 15','i5 8gb 265gb', 950000,"UrlImagen", 150443, 5);
+productos.addProduct('HP Gamer','i7 8gb 512gb 4060ti', 750000,"UrlImagen", 150453, 3);
 
-console.log(productos.getProducts()); */
 
-let opcion = prompt("Ingrese una opción");
+let opcion = parseInt(prompt("Ingrese una opción: 1)Agregar producto\n2)Mostrar productos\n3)Buscar producto por su id\n4)Salir"));
 while(opcion != 4){
-    opcion = prompt("Ingrese una opción");
+
+    switch(opcion){
+        case 1:
+        const title=prompt("Ingrese el título del producto:");
+        const description=prompt("Ingrese la descripción:")
+        const price=parseFloat(prompt("Ingrese el precio:"));
+        const thumbnail=prompt("Ingrese la ruta de imagen:");
+        const code=prompt("Ingrese el código:");
+        const stock=parseInt(prompt("Ingrese la cantidad:"));
+        productos.addProduct(title, description, price, thumbnail, code, stock);
+        break;
+        
+        case 2:
+            const prodsArray = productos.getProducts();
+            prodsArray.length ? prodsArray.forEach(product => {
+                console.log(`id - ${product.id}\ntitle - ${product.title}\ndescription - ${product.description}\nprice - $${product.price}\ncode - ${product.code}\nstock - ${product.stock}`)
+            }) :
+             console.log("No hay productos.");
+        break;
+
+        case 3:
+            const idProd = parseInt(prompt("Ingrese el id del producto:"));
+            productos.getProductById(idProd);
+        break;
+    }
+
+    opcion = parseInt(prompt("Ingrese una opción: 1)Agregar producto\n2)Mostrar productos\n3)Buscar producto por su id\n4)Salir "));
 } 
