1a295c54204a8c1f5662ecef5a67264075884d87
Lucas Mendoza <mendozalucas001@gmail.com>
2023-12-28 20:54:57 -0300

    updateProduct + cambios

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const fs = require('fs');
+const fs = require('fs'); 
 
 class ProductManager{
     static id = 0;
@@ -7,12 +7,78 @@ class ProductManager{
         this.path = path;
     }
 
+    updateProduct(id, campo, valor){
+        return fs.promises.readFile(this.path, 'utf-8')
+            .then(data => {
+                const productos = JSON.parse(data)
+                if(campo!="obj"){//si se pasa el campo, lo modifico
+                    const prodsFiltrados = productos.map((prod) => {
+                        if(prod.id === id){
+                            return{
+                                ...prod,
+                                [campo]: valor,
+                            }
+                        }else{
+                            return prod;
+                        }
+                    })
+                    //guardo en fs los prodsFiltrados
+                    return fs.promises.writeFile(this.path, JSON.stringify(prodsFiltrados, null, '\t'))
+                        .then(() => {
+                            console.log("Prods actualizados");
+                        })
+                        .catch(err => {
+                            console.log(err);
+                        })
+                }else{//se pasa el objeto entero
+                    if(typeof valor === 'object' && valor !== null){
+                        //saco el producto con el id pasado como parámetro del storage
+                        const prodsFiltrados2 = productos.filter((prod) => prod.id !== id);
+
+                        //actualizo el storage sin el producto
+                        fs.promises.writeFile(this.path, JSON.stringify(prodsFiltrados2, null, '\t'))
+                        .then(() => {
+                            console.log("Prods actualizados");
+                        })
+                        .catch(err => {
+                            console.log(err);
+                        })
+
+                        //creo un nuevo producto, manteniendo el id
+                        const newProd = {
+                            ...valor,
+                            id: id,
+                        }
+                        ProductManager.id++;
+
+                        //agrego el producto al array
+                        prodsFiltrados2.push(newProd);
+                        //guardo los productos
+                        fs.promises.writeFile(this.path, JSON.stringify(prodsFiltrados2, null, '\t'))
+                        .then(() => {
+                            console.log("Prods actualizados");
+                        })
+                        .catch(err => {
+                            console.log(err);
+                        })
+                    }
+                }
+            })
+    }
+
     deleteProduct(id){
         return fs.promises.readFile(this.path, 'utf-8')
             .then(data => {
                 const productos = JSON.parse(data);
                 const prodsFiltrados = productos.filter((prod) => prod.id !== id); 
-                return fs.promises.writeFile(this.path, JSON.stringify(prodsFiltrados, null, '\t')); 
+                return fs.promises.writeFile(this.path, JSON.stringify(prodsFiltrados, null, '\t'))
+                    .then(
+                        console.log("Prods actualizados")
+                    )
+                    .catch(error =>{
+                        console.log(error)
+                        }
+                    )
             })
             .catch(error => {
                 console.log(error);
@@ -30,7 +96,6 @@ class ProductManager{
                 console.log(error);
             })
     }
-
 
     addProduct(title, description, price, thumbnail, code, stock){
         //Leo el archivo para obtener el mayor id
@@ -86,9 +151,20 @@ const productos = new ProductManager('productos.json'); /*
 productos.addProduct('Acer 15','i5 8gb 265gb', 950000,"UrlImagen", 150443, 5);
 productos.addProduct('HP Gamer','i7 8gb 512gb 4060ti', 750000,"UrlImagen", 150453, 3); */
 
-console.log("================================================================================")
+productos.updateProduct(10, "obj", 	{
+    "id": 10,
+    "title": "Hola",
+    "thumbnail": "https://img1.cgtrader.com/items/3323407/83e613fb3a/harry-potter-print-ready-3d-model-3d-model-obj-stl-ztl.jpg",
+    "description": "HarryPotter",
+    "price": 2529.51,
+    "stock": 15,
+    "code": 1016
+});
 
-productos.getProducts().then(productos => {
+productos.updateProduct(6,"stock", 20);
+
+//Pruebas
+/* productos.getProducts().then(productos => {
     console.log("getProducts:");
     console.log(productos);
 })
@@ -99,8 +175,11 @@ productos.getProducts().then(prods => {
 
 productos.addProduct('Acer 15','i5 8gb 265gb', 950000,"UrlImagen", 152443, 5).then(retornoAdd => {
     console.log(retornoAdd)
-})
+}) 
 
+productos.getProducts().then(productos => {
+    console.log("Productos:",productos);
+})*/
 /* let opcion = parseInt(prompt("Ingrese una opción:\n1)Agregar producto\n2)Mostrar productos\n3)Buscar producto por su id\n4)Salir"));
 while(opcion != 4){
 
