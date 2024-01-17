468804634c5ac90dc451dbaa4bd5cf5448e589cb
Lucas <mendozalucas001@gmail.com>
2024-01-17 04:59:42 -0300

    avance products routes

--- routes/products.routes.js
+++ routes/products.routes.js
@@ -5,7 +5,8 @@ const productManager = new ProductManager('./productos.json')
 
 const routerProduct = Router()
 
-app.get("/products", async (req, res) => {
+//última pre entrega
+/* app.get("/products", async (req, res) => {
     try {
       let limit = req.query.limit;
       let productos = await product.getProducts();
@@ -19,8 +20,33 @@ app.get("/products", async (req, res) => {
       console.error("Error fetching products:", error);
       res.status(500).send("Internal Server Error");
     }
-});
+}); */
+//========================
 
 routerProduct.get('/', async(req, res) => {
-        
+    const products = await productManager.getProducts()
+    const limit = req.query.limit
+
+    if(limit){
+      products = products.slice(0, limit)
+    }
+
+    products ? res.status(200).send(products) : res.status(400).send('No se encontraron productos')
 })
+
+routerProduct.get('/:pid', async(req, res) => {
+  const id = req.params.pid
+  const productFound = await productManager.getProductById(id)
+  productFound ? res.status(200).send(productFound) : res.status(400).send('No se encontró el producto')
+})
+
+
+routerProduct.post('/', async(req, res) => {
+  //obtengo el producto de req.body
+})
+
+routerProduct.delete('/:pid', async(req, res) => {
+  const id = req.params.pid
+  
+})
+
