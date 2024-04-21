e362fda65bf0aaa2177ea2631f8c99ff3db32499
Lucas <mendozalucas001@gmail.com>
2024-04-21 19:49:19 -0300

    Cambios

--- src/routes/session.routes.js
+++ src/routes/session.routes.js
@@ -1,8 +1,19 @@
 const express = require('express')
 const routerSession = express.Router();
+const CartManager = require('../controller/cartManager.js')
+const cartManager = new CartManager()
 
-routerSession.get('/current', (req, res)=>{
-    res.send(req.session.passport)
+const ProductManager = require('../controller/productManager.js')
+const productManager = new ProductManager()
+
+routerSession.get('/current', async(req, res)=>{
+    const cart = await cartManager.getCartById(req.session.passport.user.cartId)
+    const user = req.session.passport.user
+    console.log('cart:', cart)
+    res.render('current',{
+        user: user,
+        cart: cart.products
+    }) 
 })
 
 module.exports = { routerSession }
