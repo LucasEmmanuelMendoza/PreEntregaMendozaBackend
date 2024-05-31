ce55c1f3c63f66a6fa25046e4bd437b2261a81b3
Lucas Mendoza <mendozalucas001@gmail.com>
2024-05-30 22:26:37 -0300

    cambios

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -51,21 +51,14 @@ routerAuth.post('/register', passport.authenticate('register', {failureRedirect:
     console.log(req.session.user)
     res.redirect('/views/login-view')
 })
+
 routerAuth.get('/failRegister', (req, res) => {
     res.send('Failed register')
 })
 
 routerAuth.post('/login', passport.authenticate('login', {failureRedirect:'/auth/failLogin', successRedirect:'/auth/successLogin'}), async(req, res)=>{
-    const datos = req.body
     res.redirect('/views/products')
 }) 
-
-/*//login jwt//
- routerAuth.post('/login', passport.authenticate('jwt', { session: false, failureRedirect: '/auth/failLogin' }), async (req, res) => {
-    const token = generaToken(req.user);
-    res.cookie("coderSecret2", token, { httpOnly: true})    
-    res.redirect('/views/products')
-})*/
 
 routerAuth.get('/successLogin', (req, res) => {
     req.session.user = req.user.first_name
@@ -75,9 +68,18 @@ routerAuth.get('/successLogin', (req, res) => {
     }
     res.redirect('/views/profile-view')
 })
+
 routerAuth.get('/failLogin', (req, res) => {
     res.send('Failed login')
 })
+
+/*//login jwt//
+ routerAuth.post('/login', passport.authenticate('jwt', { session: false, failureRedirect: '/auth/failLogin' }), async (req, res) => {
+    const token = generaToken(req.user);
+    res.cookie("coderSecret2", token, { httpOnly: true})    
+    res.redirect('/views/products')
+})*/
+
 
 routerAuth.get('/logout', async(req, res) => {
     req.session.destroy(error => {
@@ -85,7 +87,6 @@ routerAuth.get('/logout', async(req, res) => {
     })
     res.redirect('/views/login-view')
 }) 
-
 
 /*//jwt
 routerAuth.get('/logout', async(req, res) => {
