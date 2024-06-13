0d3136f2ba36ec7e3b3390d131689dc7219d0c28
Lucas <mendozalucas001@gmail.com>
2024-06-12 21:23:00 -0300

    .

--- src/tests/Session.supertest.mjs
+++ src/tests/Session.supertest.mjs
@@ -2,7 +2,6 @@ import mongoose from "mongoose";
 import { expect } from "chai";
 import supertest from "supertest";
 
-
 const requester = supertest('http://localhost:8080')
 
 mongoose.connect('mongodb+srv://mendozalucas001:3oTPgKzoGup6Azz0@testsdb.oqpw17l.mongodb.net/testsDB')
@@ -16,10 +15,17 @@ describe('Testing Session Routes', () => {
     it('Session', async() => {
         const statusCode = 400
 
-<<<<<<< HEAD
         //const result = await requester.get('/api/sessions/current')
-        const mockUser = {}
-        const result = await requester.get('/api/sessions/current').send({
+        const mockUser = {
+           "_id": "66303d8835e8143afa9da179",
+            "first_name": "Lucas Emmanuel",
+            "last_name": "Mendoza",
+            "email": "a@gmail.com",
+            "age": 22,
+            "password": "$2b$10$mhZujsMnyUaHHn/2dFx2O.0j/2uzoInkMuc6.lWUUsdnutBeC6iMK",
+            "cartId": "66303d8835e8143afa9da177",
+            "role": "user"}
+        /*  const result = await requester.get('/api/sessions/current').send({
             "_id": "66303d8835e8143afa9da179",
             "first_name": "Lucas Emmanuel",
             "last_name": "Mendoza",
@@ -28,16 +34,14 @@ describe('Testing Session Routes', () => {
             "password": "$2b$10$mhZujsMnyUaHHn/2dFx2O.0j/2uzoInkMuc6.lWUUsdnutBeC6iMK",
             "cartId": "66303d8835e8143afa9da177",
             "role": "user"
-          })
+        })*/
 
-        console.log('result:', result)
-        
         expect(result.statusCode).is.eql(200)
-=======
+
         const result = await requester.get('/api/sessions/current')
 
-        expect(result.statusCode).to.be.deep.equal(statusCode)
->>>>>>> aa1f64839e4425d515f9edbfbf4e16422320da33
-    })
+        console.log('result:', result)
 
+        expect(result.statusCode).to.be.deep.equal(statusCode)
+    })
 })
